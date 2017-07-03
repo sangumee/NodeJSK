28ca2875d1d925f95205a9dd1fa740a56624ab88
Si Hyeong Lee <super2451894@naver.com>
2017-07-04 01:15:36 +0900

    NODEJSK

--- etc/Project BK/15. MySQL With PHP/process.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("sql211.epizy.com", "epiz_20192036", sihung84265);
-mysqli_select_db($conn, "epiz_20192036_study");
-$sql = "INSERT INTO topic (title,description,author,created) VALUES('".$_POST['title']."', '".$_POST['description']."', '".$_POST['author']."', now())";
-$result = mysqli_query($conn, $sql);
-header('Location: index.php');
-?>
--- etc/Project BK/MySQL PHP Board/data/db_info.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $SERV = "sql211.epizy.com";
-    $USER = "epiz_20192036";
-    $PASS = "sihung84265";
-    $DBNM = "epiz_20192036_study";
- ?>
